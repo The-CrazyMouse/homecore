d652a77e0616f9dd7b18e6467e4095bb73d04a5d
Crazy Mouse <andrerddantas@hotmail.com>
2025-10-29 11:34:08 +0000

    starting a pre-install module

--- src/pre-install/root.zig
+++ src/pre-install/root.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 const print = std.debug.print;
 
+fn check_arch() !void {}
+
 pub fn pre_install() !void {
     // check arch
     // check pacman
-    //
+    // ping
+    // get sudo
 }
